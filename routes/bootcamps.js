4a54c20787a0babc7be1422b337499152e9fa83d
Son Tran <son.tranngoc@gmail.com>
2024-07-24 16:05:35 +0700

    Bootcamp route and controller

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,33 +1,22 @@
 const express = require('express');
-
 const router = express.Router();
+const bootcamps = require('../controllers/bootcamps');
 
-router.get('/', (req, res) => {
-  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
-});
+const {
+  getBootcamps,
+  getBootcamp,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+} = bootcamps;
 
-router.get('/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Show bootcamp ${req.params.id}` });
-});
+router.route('/').get(getBootcamps);
 
-router.post('/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Create a bootcamp ${req.params.id}` });
-});
-
-router.put('/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Update a bootcamp ${req.params.id}` });
-});
-
-router.delete('/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Delete a bootcamp ${req.params.id}` });
-});
+router
+  .route('/:id')
+  .get(getBootcamp)
+  .post(createBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
 
 module.exports = router;
