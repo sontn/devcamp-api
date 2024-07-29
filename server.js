b41a404941c77bebdbc5c11bfbab3f517df1d2cd
Son Tran <son.tranngoc@gmail.com>
2024-07-29 09:45:49 +0700

    Configure express from scratch n4

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 const express = require('express');
-const dotenv = require('dotenv');
-const bootcamp = require('./routes/bootcamps');
 
-dotenv.config({
-  path: './config/config.env',
-});
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
 const PORT = process.env.PORT;
-
-app.use('/api/v1/bootcamps', bootcamp);
-
 app.listen(
   PORT,
   console.log(`Server running at mode ${process.env.NODE_ENV} on port ${PORT}`)
