14126fc4c0e3ecb25e80d47ae2b02b0c9c43ad75
Son Tran <son.tranngoc@gmail.com>
2024-08-06 15:34:34 +0700

    Fetch bootcamps

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,15 +3,30 @@ const Bootcamp = require('../models/Bootcamp');
 // @desc        Get all bootcamps
 // @route       GET /api/v1/bootcamps
 // @access      Public
-exports.getBootcamps = (req, res, next) => {
-  res.send(`Get all bootcamps`);
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find();
+    res.status(200).json({ success: true, data: bootcamps });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc        Get single bootcamp
 // @route       GET /api/v1/bootcamps/:id
 // @access      Public
-exports.getBootcamp = (req, res, next) => {
-  res.send(`Get single bootcamp with id ${req.params.id}`);
+exports.getBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+    if (!bootcamp) {
+      return res
+        .status(404)
+        .json({ success: false, error: 'Bootcamp not found' });
+    }
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc        Create a bootcamp
