21cebae42fdaf3d3f36aad7810f773043ce7f6e1
Son Tran <son.tranngoc@gmail.com>
2024-08-06 11:23:59 +0700

    Create route, controller for bootcamps times 10

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,34 +1,34 @@
-// @desc    Get all bootcamps
-// @route   GET /api/v1/bootcamps
-// @access  Public
+// @desc        Get all bootcamps
+// @route       GET /api/v1/bootcamps
+// @access      Public
 exports.getBootcamps = (req, res, next) => {
   res.send(`Get all bootcamps`);
 };
 
-// @desc    Get single bootcamps
-// @route   GET /api/v1/bootcamps/:id
-// @access  Public
+// @desc        Get single bootcamp
+// @route       GET /api/v1/bootcamps/:id
+// @access      Public
 exports.getBootcamp = (req, res, next) => {
   res.send(`Get single bootcamp with id ${req.params.id}`);
 };
 
-// @desc    Create a bootcamp
-// @route   POST /api/v1/bootcamps
-// @access  Private
+// @desc        Create a bootcamp
+// @route       POST /api/v1/bootcamps
+// @access      Private
 exports.createBootcamp = (req, res, next) => {
   res.send(`Create a bootcamp`);
 };
 
-// @desc    Update a bootcamp
-// @route   PUT /api/v1/bootcamps/:id
-// @access  Private
+// @desc        Update a bootcamp
+// @route       PUT /api/v1/bootcamps/:id
+// @access      Private
 exports.updateBootcamp = (req, res, next) => {
   res.send(`Update a bootcamp with id ${req.params.id}`);
 };
 
-// @desc    Delete a bootcamp
-// @route   DELETE /api/v1/bootcamps/:id
-// @access  Private
+// @desc        Delete a bootcamp
+// @route       DELETE /api/v1/bootcamps/:id
+// @access      Private
 exports.deleteBootcamp = (req, res, next) => {
   res.send(`Delete a bootcamp with id ${req.params.id}`);
 };
--- server.js
+++ server.js
@@ -6,9 +6,9 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-const PORT = process.env.PORT;
-
 app.use('/api/v1/bootcamps', bootcamps);
+
+const PORT = process.env.PORT;
 
 app.listen(
   PORT,
