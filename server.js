28859fe0626443e466df5fd823d47da5340b9da4
Son Tran <son.tranngoc@gmail.com>
2024-08-07 15:57:26 +0700

    Create route, controller for bootcamp times 3

--- server.js
+++ server.js
@@ -1,35 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const bootcamps = require('./routes/bootcamps');
-const connectDB = require('./config/db');
-// const logger = require('./middleware/logger');
-const morgan = require('morgan');
-const colors = require('colors');
 
 dotenv.config({ path: './config/config.env' });
 
-connectDB();
-
 const app = express();
-app.use(express.json());
-
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
-
-app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT;
 
-const server = app.listen(
-  PORT,
-  console.log(
-    `Server running at mode ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
-  )
-);
+app.use('/api/v1/bootcamps', bootcamps);
 
-process.on('unhandledRejection', (err, promise) => {
-  console.log('Come here---------------');
-  console.log(`Error: ${err.message}`);
-  server.close(() => process.exit(1));
-});
+app.listen(
+  PORT,
+  console.log(`Server running at mode ${process.env.NODE_ENV} on port ${PORT}`)
+);
