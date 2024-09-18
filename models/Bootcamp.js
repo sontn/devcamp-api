5a911062a400d1afad8e0fdb2981f4459400e329
Son Tran <son.tranngoc@gmail.com>
2024-09-18 11:30:02 +0700

    CRUD for bootcamps for a long time to do

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -3,8 +3,6 @@ const BootcampSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'Please add a name'],
-    unique: true,
-    maxlength: [50, 'Name is not more than 50 characters'],
   },
 });
 
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,14 +1,19 @@
 const express = require('express');
-const router = express.Router();
 const {
-  getBoocamps,
-  createBoocamp,
-  getBoocamp,
-  updateBoocamp,
-  deleteBoocamp,
+  getBootcamps,
+  getBootcamp,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
 } = require('../controllers/bootcamps');
 
-router.route('/').get(getBoocamps).post(createBoocamp);
-router.route('/:id').get(getBoocamp).put(updateBoocamp).delete(deleteBoocamp);
+const router = express.Router();
+
+router.route('/').get(getBootcamps).post(createBootcamp);
+router
+  .route('/:id')
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
 
 module.exports = router;
