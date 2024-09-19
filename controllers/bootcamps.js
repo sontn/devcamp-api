b6dd0b1d8c49b9a72861898f51829fd384cbfed5
Son Tran <son.tranngoc@gmail.com>
2024-09-19 16:17:46 +0700

    Fix typo and runValidators

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,10 +1,9 @@
-// @desc        Get all bootcamps
-// @route       GET /api/v1/bootcamps
-
 const Bootcamp = require('../models/Bootcamp');
 
+// @desc        Get all bootcamps
+// @route       GET /api/v1/bootcamps
 // @access      Public
-exports.getBoocamps = async (req, res, next) => {
+exports.getBootcamps = async (req, res, next) => {
   try {
     const bootcamps = await Bootcamp.find();
     res.status(200).json({ success: true, data: bootcamps });
@@ -16,7 +15,7 @@ exports.getBoocamps = async (req, res, next) => {
 // @desc        Get single bootcamp
 // @route       GET /api/v1/bootcamps/:id
 // @access      Public
-exports.getBoocamp = async (req, res, next) => {
+exports.getBootcamp = async (req, res, next) => {
   try {
     const bootcamp = await Bootcamp.findById(req.params.id);
     if (!bootcamp) {
@@ -36,7 +35,7 @@ exports.getBoocamp = async (req, res, next) => {
 // @desc        Create a bootcamp
 // @route       POST /api/v1/bootcamps
 // @access      Private
-exports.createBoocamp = async (req, res, next) => {
+exports.createBootcamp = async (req, res, next) => {
   try {
     const bootcamp = await Bootcamp.create(req.body);
     res.status(201).json({ success: true, data: bootcamp });
@@ -48,11 +47,11 @@ exports.createBoocamp = async (req, res, next) => {
 // @desc        Update a bootcamp
 // @route       PUT /api/v1/bootcamps/:id
 // @access      Private
-exports.updateBoocamp = async (req, res, next) => {
+exports.updateBootcamp = async (req, res, next) => {
   try {
     const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
-      runValidator: true,
+      runValidators: true,
     });
     if (!bootcamp) {
       return res
@@ -71,7 +70,7 @@ exports.updateBoocamp = async (req, res, next) => {
 // @desc        Delete a bootcamp
 // @route       DELETE /api/v1/bootcamps/:id
 // @access      Private
-exports.deleteBoocamp = async (req, res, next) => {
+exports.deleteBootcamp = async (req, res, next) => {
   try {
     const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
     if (!bootcamp) {
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const {
-  getBoocamps,
-  createBoocamp,
-  getBoocamp,
-  updateBoocamp,
-  deleteBoocamp,
+  getBootcamps,
+  createBootcamp,
+  getBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
 } = require('../controllers/bootcamps');
 
-router.route('/').get(getBoocamps).post(createBoocamp);
-router.route('/:id').get(getBoocamp).put(updateBoocamp).delete(deleteBoocamp);
+router.route('/').get(getBootcamps).post(createBootcamp);
+router
+  .route('/:id')
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
 
 module.exports = router;
