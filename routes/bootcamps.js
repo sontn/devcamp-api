8406a8cbd4cae3587ce2c09a492131d56eae4af7
Son Tran <son.tranngoc@gmail.com>
2024-11-05 14:26:32 +0700

    Init

--- routes/bootcamps.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getBootcamps } = require('../controllers/bootcamps');
-
-router.route('/').get(getBootcamps);
-
-module.exports = router;
--- server.js
+++ server.js
@@ -1,18 +1,1 @@
 const express = require('express');
-const dotenv = require('dotenv');
-const bootcamps = require('./routes/bootcamps');
-const connectDB = require('./config/db');
-
-dotenv.config({ path: './config/config.env' });
-
-const PORT = process.env.PORT;
-connectDB();
-
-const app = express();
-
-app.use('/api/v1/bootcamps', bootcamps);
-
-app.listen(
-  PORT,
-  console.log(`Server running at mode ${process.env.NODE_ENV} on port ${PORT}`)
-);
