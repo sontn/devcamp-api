919702d20df72c93f529a96bace761a122381f28
Son Tran <son.tranngoc@gmail.com>
2024-11-14 09:38:32 +0700

    Practice makes perfect 17

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,10 +1,9 @@
 const Bootcamp = require('../models/Bootcamp');
-
 exports.getBootcamps = async (req, res) => {
   try {
     const bootcamps = await Bootcamp.find();
     res.status(200).send({ success: true, data: bootcamps });
   } catch (err) {
-    res.status(400).send({ success: false, error: 'False' });
+    // res.status(200).send({ success: true, data: 'This is this' });
   }
 };
