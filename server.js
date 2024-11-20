68af2c28091cfb64d17e76e22d95299ece41e187
Son Tran <son.tranngoc@gmail.com>
2024-11-20 09:48:36 +0700

    Practices makes perfect 25

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const connectDB = require('./config/db');
 const bootcamps = require('./routes/bootcamps');
+const connectDB = require('./config/db');
 
 dotenv.config({ path: './config/config.env' });
 
